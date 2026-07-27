54d6102ced046d9657464e0677964988e04fc5a1
Chuya <245904786+Chuya-WD@users.noreply.github.com>
2026-07-27 15:38:54 -0500

    crear estructura HTML de la calculadora

--- script.js
+++ script.js
@@ -1,17 +1,36 @@
-function add(a, b){
-    return a+b; 
+function add(a, b) {
+    return a + b;
 }
 
-function subtract(a,b){
-    return a-b;
+function subtract(a, b) {
+    return a - b;
 }
 
-function multiply(a, b){
-    return a*b;
+function multiply(a, b) {
+    return a * b;
 }
 
-function divide(a, b){
-    if(b === 0)
-        return "No existe división para cero, chico"; 
-    return a/b;
+function divide(a, b) {
+    if (b === 0)
+        return "No existe división para cero, chico";
+    return a / b;
+}
+
+function operate(operator, a, b) {
+
+    switch (operator) {
+        case "+":
+            return add(Number(a), Number(b)); 
+            break;
+        case "-":
+            return subtract(Number(a), Number(b)); 
+            break;
+        case "x":
+            return multiply(Number(a), Number(b)); 
+
+            break;
+        case "+":
+             return divide(Number(a), Number(b)); 
+            break;
+    }
 }
