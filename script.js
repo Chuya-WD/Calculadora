ac0fd9d1b555ff2a79630c1b5831615f475acaaf
Chuya <245904786+Chuya-WD@users.noreply.github.com>
2026-07-27 20:26:50 -0500

    implementar captura de digitos y actualizacion de de pantalla2

--- script.js
+++ script.js
@@ -20,17 +20,44 @@ function operate(operator, a, b) {
 
     switch (operator) {
         case "+":
-            return add(Number(a), Number(b)); 
+            return add(Number(a), Number(b));
             break;
         case "-":
-            return subtract(Number(a), Number(b)); 
+            return subtract(Number(a), Number(b));
             break;
         case "x":
-            return multiply(Number(a), Number(b)); 
+            return multiply(Number(a), Number(b));
 
             break;
         case "+":
-             return divide(Number(a), Number(b)); 
+            return divide(Number(a), Number(b));
             break;
     }
 }
+
+let currentOperand = "0";
+let previousOperand = "";
+let operation = null;
+let shouldResetScreen = false;
+
+let botones = document.querySelectorAll(".btn.number");
+
+function appendNumber(e) {
+    let numeroPresionado = e.target.dataset.number;
+
+    if (currentOperand === "0") {
+        currentOperand = numeroPresionado;
+    } else {
+        currentOperand += numeroPresionado;
+    }
+    updateDisplay();  
+}
+
+function updateDisplay(){
+    let cO = document.querySelector(".current-operand"); 
+    cO.innerText = currentOperand; 
+}
+
+botones.forEach((boton) => {
+    boton.addEventListener("click", appendNumber);
+});
